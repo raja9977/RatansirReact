cf12da476057f3bceb30d5df07ea72a5c72f6480
Rajasekhar <147872776+raja9977@users.noreply.github.com>
2024-11-02 12:24:17 +0530

    Update AboutUs.jsx

--- React-flow-app/src/AboutUs.jsx
+++ React-flow-app/src/AboutUs.jsx
@@ -2,7 +2,7 @@ function AboutUs()
 {
     return(
         <>
-        <h1> welcome to AboutUs login....</h1>
+        <h1> welcome to AboutUs login,,</h1>
         <p> react class is good,,,,</p>
         </>
     )
